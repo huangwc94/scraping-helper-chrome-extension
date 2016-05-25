13f850ed153b75068bad693c28e0bf33d0c888ae
Weicheng Huang <huangwc94@gmail.com>
2016-05-25 15:41:46 -0400

    Alpha Version updated, model can be applied to simple and regular html structure

--- js/sh-main.js
+++ js/sh-main.js
@@ -10,10 +10,11 @@ $(function(){
 		e.stopPropagation();
 		
 		
-		var path = $(this).getFullPath(true);
+		var path = $(this).getFullPathName(true);
+		var selected_node = $(this);
 		//console.log(path);
 		$($(this).prop("tagName").toLowerCase()).each(function(){
-			var this_path = $(this).getFullPath(true);
+			var this_path = $(this).getFullPathName(true);
 			if(this_path == path){
 				$(this).css("border","soild");
 				$(this).addClass("sh-hover");
@@ -21,7 +22,7 @@ $(function(){
 		});
 		// $(this).addClass("sh-tooltip");
 		// $(this).attr("data-tip","选择提示 : "+path.toLowerCase());
-		updateSuggestion(path,$(this).attr("class"));
+		updateSuggestion();
 		
 	});
 	$("*").click(function(e){
@@ -41,47 +42,125 @@ $(function(){
 			
 			
 		}else{
-			$(this).addClass("sh-select");
+			if($(this).prop("tagName") == $(".sh-hover").prop("tagName") || $(this).prop("tagName") == $(".sh-select").prop("tagName")){
+				$(this).addClass("sh-select");
+			}
+			
 		}
-		$(".sh-hover").addClass("sh-select");
-		removeAllElementClass("sh-hover");
-		var path = $(this).getFullPath(true);
-		var suggestion = $(this).attr("class");
-		updateSuggestion(path,suggestion);
+		$(".sh-hover").addClass("sh-select").removeClass("sh-hover");
+		
+		updateSuggestion();
 		
 	});
+	addPanel();
+	function addPanel(){
+		$("body").append("<div id='sh-panel' class='sh-move-to-right'><h4>内容选择器控制面板</h4><ul><li><button id='sh-action-reset'>重新选择</button></li><li><button  id='sh-action-copy-selector'>拷贝选择器</button></li><li><button  id='sh-action-copy-content'>拷贝内容</button></li><div id='sh-suggestion'></div></div>");
 
-	$("body").append("<div id='sh-clear' class='sh-move-to-right'>重新选择<div class='sh-suggestion '></div></div>");
-	$("#sh-clear").click(function(e){
-		e.stopPropagation();
-		removeAllElementClass("sh-select");
-		state = 0;
-	});
+		$("#sh-action-reset").click(function(e){
+			e.stopPropagation();
+			removeAllElementClass("sh-select");
+			state = 0;
+		});
 
-	function updateSuggestion(path,suggestion){
-		$(".sh-suggestion").html("元素路径："+path.toLowerCase()+"<br>选择提示："+suggestion);
+		$("#sh-action-copy-selector").click(function(e){
+			e.stopPropagation();
+			
+			
+			
+			$(".modal").text($("#sh-suggestion-code").text()).modal();
+		});
+
+		$("#sh-action-copy-content").click(function(e){
+			e.stopPropagation();
+			var data = "";
+			$(".sh-select").each(function(){
+				data += $(this).text();
+			});
+			$(".modal").text(data).modal();
+		});
+	}
+	
+
+	function updateSuggestion(){
+		apporach1_BreadthFirstSearch();
 	}
 
+	function apporach1_BreadthFirstSearch(){
+		var currentUseingClassName = state === 0 ? "sh-hover" : "sh-select";
+		console.log("============================================================");
+		var currentSelected = $("."+currentUseingClassName);
+
+		var path;
+		var count = currentSelected.length;
+		var suggestion = "";
+		var findSolution = false;
+		if(currentSelected.length>0){
+			path = currentSelected.getFullPathName(true);
+
+			findSolution = true;
+			var currentNode = $(currentSelected[0]);
+			suggestion = "";
+			while(!isCorrectSelection(currentSelected,suggestion) && currentNode.prop("tagName") !== "BODY" ){
+				findSolution = false;
+				console.log(currentNode);
+				var classList = currentNode.attr('class')===undefined ? [] :currentNode.attr('class').split(/\s+/);
+
+				// loop though all possible class attritube to see if we are able to succfully select 
+				console.log("Current Node Class List"+currentNode.attr('class'));
+				$.each(classList, function(index, item) {
+					
+					if(item == 'sh-select' || item == 'sh-hover' || item == ''){
+						return;
+					}
+					console.log("Using class:"+item);
+					var trival_solution = currentNode.prop("tagName").toLowerCase()+"."+item +" "+  suggestion;
+					if(isCorrectSelection(currentSelected,trival_solution)){
+						findSolution = true;
+						suggestion = trival_solution;
+						console.log("Solution Found!");
+						return false;
+					}
+				});
+				if(findSolution){
+					break;
+				}
+				console.log("go to upper level tree for answer");
+				suggestion = currentNode.prop("tagName").toLowerCase() + " "+suggestion;
+				currentNode = currentNode.parent();
+			}
+		}
+		
+		if(!findSolution){
+			suggestion = "找不到合适的选择器！请重试";
+		}
+		$("#sh-suggestion").html("元素路径："+path.toLowerCase()+"<br>选择数量："+count+"<br>选择器提示：<span id='sh-suggestion-code'>"+suggestion+"</span>");
+	}
+
+	function isCorrectSelection(currentSelected,selector){
+		var selectorResult = $(selector);
+		console.log("---Testing:"+selector+'---');
+		console.log(currentSelected);
+		console.log(selectorResult);
+		console.log("--------------------------");
+		if (currentSelected.length != selectorResult.length){
+
+			return false;
+		}
+
+		// for(var i = 0 ; i < currentSelected ; i++ ){
+		// 	if (selectorResult[i] != currentSelected[i]){
+		// 		return false;
+		// 	}
+		// }
+		return true;
+		
+	}
 	function removeAllElementClass(className){
 		$("."+className).removeClass(className);
 	}
 
-	$.fn.extend({
-        getFullPath: function(stopAtBody){
-            stopAtBody = stopAtBody || false;
-            function traverseUp(el){
-                var result = el,
-                    pare = $(el).parent()[0];
-                if (pare.tagName !== undefined && (!stopAtBody || pare.tagName !== 'BODY')){
-                    result = [traverseUp(pare), result];
-                }                
-                return result;
-            };
-            return this.length > 0 ? traverseUp(this[0]) : '';
-        }
-    });
     $.fn.extend({
-        getFullPath: function(stopAtBody){
+        getFullPathName: function(stopAtBody){
             stopAtBody = stopAtBody || false;
             function traverseUp(el){
                 var result = el.tagName,
