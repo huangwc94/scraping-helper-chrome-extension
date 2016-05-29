9101b3bfcd0ae788be117b0e94ab38dc6f64956e
Weicheng Huang <huangwc94@gmail.com>
2016-05-28 23:33:01 -0400

    bug fix

--- js/sh-iframe.js
+++ js/sh-iframe.js
@@ -1,5 +1,6 @@
 $(function(){
-	$('.sh-hide-on-collpse').hide();
+
+
 	window.addEventListener('message', messageHandler);
 	function messageHandler(e){
 		console.log(e.data);
@@ -11,9 +12,9 @@ $(function(){
 	}
 	$("#sh-action-reset").click(function(e){
 			e.stopPropagation();
-			
+
 			parent.postMessage({type:"reset"},"*");
-			
+
 			$("#sh-suggestion-count").html("选择数量：<span class='label label-danger'>0/0</span>");
 			$("#sh-suggestion-path").html("元素路径：<kbd></kbd>");
 			$("#sh-suggestion-recommend").html("建议选择：<code></code>");
@@ -29,12 +30,12 @@ $(function(){
 	$("#sh-action-detail").click(function(e){
 		e.stopPropagation();
 
-		
+
 		if(state === 0){
 			setTimeout(function(){
 				$('.sh-hide-on-collpse').show();
 			},600);
-			
+
 			$(this).html("关闭详情");
 			state = 1;
 			parent.postMessage({type:"expand"},"*");
@@ -44,9 +45,9 @@ $(function(){
 			state = 0;
 			parent.postMessage({type:"collpse"},"*");
 		}
-		
-	});
 
+	});
+	$('.sh-hide-on-collpse').hide();
 	$(".sh-action-view").click(function(){
 		$(".sh-hide").hide();
 		$("#sh-content-"+$(this).attr("data")).show();
@@ -61,10 +62,10 @@ $(function(){
     console.log("iframe init successful");
 	function updatePanel(data){
 
-		
+
 		var isFound = data.findSolution? "label label-success" : "label label-danger";
-		
-		
+
+
 		$("#sh-suggestion-count").html("选择数量：<span class='"+isFound+"'>"+data.count.select + "/" +data.count.predict+"</span>");
 		$("#sh-suggestion-path").html("元素路径：<kbd>"+data.path+"</kbd>");
 		$("#sh-suggestion-recommend").html("建议选择：<code>"+data.recommend+"</code>");
@@ -72,10 +73,10 @@ $(function(){
 		$("#sh-suggestion-suggestion").html("备用选择：<code></code>");
 		if(!data.findSolution)
 			$("#sh-suggestion-suggestion").html("备用选择：<code>"+data.suggestion+"</code>");
-		
+
 
 		$("#sh-modal-content").html(
-			  "<div class='sh-hide' id='sh-content-text'>"  + data.modal.text_ + "</div>" 
+			  "<div class='sh-hide' id='sh-content-text'>"  + data.modal.text_ + "</div>"
 			+ "<div class='sh-hide' id='sh-content-href'>"  + data.modal.href_ + "</div>"
 			+ "<div class='sh-hide' id='sh-content-src'>"   + data.modal.src_ + "</div>"
 			+ "<div class='sh-hide' id='sh-content-class'>" + replaceAll(data.modal.class_,"sh-select", "") + "</div>"
@@ -91,7 +92,7 @@ $(function(){
 		$("."+className, window.parent.document).removeClass(className);
 	}
 	function replaceAll(string,search, replacement) {
-    
+
     	return string.split(search).join(replacement);
 	};
 });
